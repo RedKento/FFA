8616387a4044f3f3e08157d074dc5670e4a14ed7
Luner <kyleroe94@gmail.com>
2018-05-03 18:25:51 -0500

    unused

--- src/Program.cs
+++ src/Program.cs
@@ -9,7 +9,6 @@ using MongoDB.Bson;
 using MongoDB.Driver;
 using Newtonsoft.Json;
 using System;
-using System.Collections.Generic;
 using System.IO;
 using System.Reflection;
 using System.Threading;
